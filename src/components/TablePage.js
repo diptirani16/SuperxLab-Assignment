42fe015917ce72f45ca70c334988bf18a5ec2618
Dipti Rani <gdipti642@gmail.com>
2021-10-15 11:42:37 +0530

    update

--- src/components/TablePage.js
+++ src/components/TablePage.js
@@ -7,29 +7,55 @@ class TablePage extends Component {
         super(props);
         this.state = {
             rows: [
-                {name: 'Text1', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'},
-                {name: 'Text2', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'},
-                {name: 'Text3', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'},
-                {name: 'Text4', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'}
+                {id: 0, name: 'Text1', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'},
+                {id: 1, name: 'Text2', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'},
+                {id: 2, name: 'Text3', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'},
+                {id: 3, name: 'Text4', calories: 'Text', fat: 'Text', carbs: 'Text', protein: 'Text'}
             ],
+            index: 0
         }
         
         this.handleChange = this.handleChange.bind(this);
-        // this.handleName = this.handleName.bind(this);
-        // this.handleCalories = this.handleCalories.bind(this);
-        // this.handleFat = this.handleFat.bind(this);
-        // this.handleCarbs = this.handleCarbs.bind(this);
-        // this.handleProtein = this.handleProtein.bind(this);
+        this.handleName = this.handleName.bind(this);
+        this.handleCalories = this.handleCalories.bind(this);
+        this.handleFat = this.handleFat.bind(this);
+        this.handleCarbs = this.handleCarbs.bind(this);
+        this.handleProtein = this.handleProtein.bind(this);
     }
 
 
     handleChange(id) {
+        console.log(id)
         this.state.rows.map(i => {
-            if(i.name === id){
-                let indexOfOldData = this.state.rows.indexOf(i); 
-                console.log(indexOfOldData)
+            if(i.id === id){
+                console.log(i)
+                console.log(this.state.rows.indexOf(i))
+                this.setState({
+                    index: this.state.rows.indexOf(i)
+                })
+                console.log(this.state.index);
             }
         })
+    }
+
+    handleName(e) {
+        this.state.rows[this.state.index].name = e.target.value
+    }
+
+    handleCalories(e) {
+        this.state.rows[this.state.index].calories = e.target.value
+    }
+
+    handleFat(e) {
+        this.state.rows[this.state.index].fat = e.target.value
+    }
+
+    handleCarbs(e) {
+        this.state.rows[this.state.index].carbs = e.target.value
+    }
+
+    handleProtein(e) {
+        this.state.rows[this.state.index].protein = e.target.value
     }
 
 
@@ -58,12 +84,12 @@ class TablePage extends Component {
                         </TableHead>
                         <TableBody>
                             {this.state.rows.map((row) => (
-                                <TableRow key={row.name} onChange={() => this.handleChange(row.name)}>
-                                    <TableCell component="th" scope="row"><input type="text" value={row.name} style={inputStyle}></input> </TableCell>
-                                    <TableCell><input type="text" style={inputStyle} value={row.calories}></input></TableCell>
-                                    <TableCell><input type="text" style={inputStyle} value={row.fat}></input></TableCell>
-                                    <TableCell><input type="text" style={inputStyle} value={row.carbs}></input></TableCell>
-                                    <TableCell><input type="text" style={inputStyle} value={row.protein}></input></TableCell>
+                                <TableRow key={row.id} onChange={() => this.handleChange(row.id)}>
+                                    <TableCell component="th" scope="row"><input type="text" value={row.name} onChange={this.handleName} style={inputStyle}></input> </TableCell>
+                                    <TableCell><input type="text" style={inputStyle} onChange={this.handleCalories} value={row.calories}></input></TableCell>
+                                    <TableCell><input type="text" style={inputStyle} onChange={this.handleFat} value={row.fat}></input></TableCell>
+                                    <TableCell><input type="text" style={inputStyle} onChange={this.handleCarbs} value={row.carbs}></input></TableCell>
+                                    <TableCell><input type="text" style={inputStyle} onChange={this.handleProtein} value={row.protein}></input></TableCell>
                                 </TableRow>
                             ))}
                         </TableBody>
